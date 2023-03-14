41d8651cbb9eacae3f07ffa35f768db325a8e4d9
shrusred <reddy.shruthi35@gmail.com>
2023-03-14 15:25:11 -0400

    added the graphs for population statistics

--- frontendapp/src/components/Manager/ManagerPopulationStats/ManagerPopulationStats.js
+++ frontendapp/src/components/Manager/ManagerPopulationStats/ManagerPopulationStats.js
@@ -11,17 +11,16 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  LabelList,
 } from "recharts";
 
 function ManagerPopulationStats() {
   const { userid } = useParams();
-  // const [age, setAge] = useState([]);
-  // const [gender, setGender] = useState([]);
-  // const [yearlyincome, setYearlyIncome] = useState([]);
+  const [age, setAge] = useState([]);
+  const [yearlyincome, setYearlyIncome] = useState([]);
   // const [ethnicity, setEthnicity] = useState([]);
   // const [occupation, setOccupation] = useState([]);
-
-  console.log(userid);
+  // console.log(userid);
 
   const data = [
     { city: "Markham", age: 29 },
@@ -30,6 +29,9 @@ function ManagerPopulationStats() {
   const income = [
     { city: "Markham", income: 73300 },
     { city: "Vaughn", income: 90000 },
+    { city: "Vaughn", income: 10000 },
+    { city: "Vaughn", income: 50000 },
+    { city: "Vaughn", income: 120000 },
   ];
   function addArrayElements(arr) {
     let sum = 0;
@@ -40,65 +42,68 @@ function ManagerPopulationStats() {
 
     return sum;
   }
-
-  function getCensusData(userid) {
+  ////////////////////// AGE  ///////////////////////////////
+  function getAgebyCity(userid) {
     axios
       .get(`http://localhost:8080/manager/${userid}/census`)
       .then((response) => {
-        console.log(response.data);
         var cities = [];
-        var ethnicities = [];
-        var occupations = [];
-        var genders = [];
         var age = [];
-        var yearlyincome = [];
-        response.data.map((i) => {
-          // console.log("age", i.age);
-          // console.log(i.municipality_name);
-          cities.push(i.municipality_name);
-          genders.push(i.gender);
-          ethnicities.push(i.ethnicity);
-          occupations.push(i.occupation);
-        });
-        // console.log(cities);
-        var cities_set = new Set(cities);
-        var genders_set = new Set(genders);
-        var ethnicites_set = new Set(ethnicites);
-        var occupations_set = new Set(occupation);
 
-        const unique_cities = Array.from(cities_set);
-        const unique_genders = Array.from(genders_set);
-        const unique_ethnicities = Array.from(ethnicites_set);
-        const unique_occupations = Array.from(occupations_set);
-        console.log(unique_cities);
         response.data.map((i) => {
-          ///cities
+          cities.push(i.municipality_name);
+        });
+        //sets
+        var cities_set = new Set(cities);
+        //unique cities
+        const unique_cities = Array.from(cities_set);
+        //get metrics for cities
+        response.data.map((i) => {
           for (let j = 0; j < unique_cities.length; j++) {
             if (i.municipality_name === unique_cities[j]) {
               let newfield_age = { city: unique_cities[j], age: i.age };
+
+              age.push(newfield_age);
+            }
+          }
+        });
+        console.log("age array", age);
+        setAge(age);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  ///////////////////////// INCOME //////////////////////////////////////
+  function getIncomebyCity(userid) {
+    axios
+      .get(`http://localhost:8080/manager/${userid}/census`)
+      .then((response) => {
+        var cities = [];
+
+        var yearlyincome = [];
+        response.data.map((i) => {
+          cities.push(i.municipality_name);
+        });
+        //sets
+        var cities_set = new Set(cities);
+        //unique cities
+        const unique_cities = Array.from(cities_set);
+        //get metrics for cities
+        response.data.map((i) => {
+          for (let j = 0; j < unique_cities.length; j++) {
+            if (i.municipality_name === unique_cities[j]) {
               let newfield_yearlyincome = {
                 city: unique_cities[j],
                 yearlyincome: i.income,
               };
-              age.push(newfield_age);
               yearlyincome.push(newfield_yearlyincome);
             }
           }
-          ///gender
-          for (let k = 0; k < unique_genders.length; k++) {
-            if (i.gender === unique_genders[k]) {
-              let newfield_gender = { city: unique_genders[k], age: i.gender };
-            }
-          }
-
-          ///ethnicities
-          for (let l = 0; l < unique_ethnicities.length; l++) {}
-
-          ///occupation
-          for (let m = 0; m < unique_occupations.length; m++) {}
         });
-        console.log(age);
-        console.log(yearlyincome);
+        console.log("yearlyincome array", yearlyincome);
+        setYearlyIncome(yearlyincome);
       })
       .catch((error) => {
         console.error(error);
@@ -106,15 +111,57 @@ function ManagerPopulationStats() {
   }
 
   useEffect(() => {
-    getCensusData(userid);
+    getAgebyCity(userid);
+    getIncomebyCity(userid);
   }, []);
+
+  /////////// AVG. AGE ///////////////
+
+  let age_result = Object.entries(
+    age.reduce((acc, x) => {
+      let age = acc[x.city] || [];
+      age.push(x.age);
+      acc[x.city] = age;
+      return acc;
+    }, {})
+  ).map((x) => {
+    let city = x[0];
+    // console.log(city);
+    let ages = x[1];
+    // console.log(ages);
+    let sum = ages.reduce((a, b) => a + b, 0);
+    let avg = sum / ages.length || 0;
+    return { city: city, age: avg };
+  });
+
+  // console.log(age_result);
+
+  ////////// AVG. INCOME ///////////////
+  let income_result = Object.entries(
+    yearlyincome.reduce((acc, x) => {
+      let income = acc[x.city] || [];
+      income.push(x.yearlyincome);
+      acc[x.city] = income;
+      return acc;
+    }, {})
+  ).map((x) => {
+    let city = x[0];
+    // console.log(city);
+    let incomes = x[1];
+    // console.log(ages);
+    let sum = incomes.reduce((a, b) => a + b, 0);
+    let avg = sum / incomes.length || 0;
+    return { city: city, income: avg };
+  });
+
+  console.log("income average:", income_result);
 
   return (
     <>
       <BarChart
         width={500}
         height={300}
-        data={data}
+        data={age_result}
         margin={{
           top: 5,
           right: 30,
@@ -129,28 +176,11 @@ function ManagerPopulationStats() {
         <Legend />
         <Bar dataKey="age" fill="#8884d8" />
       </BarChart>
+      {/*************************************************/}
       <BarChart
         width={500}
         height={300}
-        data={income}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="city" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="income" fill="#82ca9d" />
-      </BarChart>
-      <BarChart
-        width={500}
-        height={300}
-        data={income}
+        data={income_result}
         margin={{
           top: 5,
           right: 30,
